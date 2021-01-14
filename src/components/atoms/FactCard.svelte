e2f57d1a744fd43d7a17824c5c6b7dbf35ba8ab3
Jonah Meijers <jonahmeijers97@gmail.com>
2021-01-14 10:09:05 +0000

    feat: add fact cards

---
+++ src/components/atoms/FactCard.svelte
@@ -0,0 +1,58 @@
+<script>
+  export let energyType = 'solar'
+  export let amount = 0
+  export let background = 'green'
+</script>
+
+<style>
+  article {
+    box-sizing: border-box;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    background: var(--bg);
+    border-radius: 0.75rem;
+    color: var(--white);
+    padding: calc(0.75 * var(--step-0));
+    width: 100%;
+    height: calc(9 * var(--step-0));
+    margin-right: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  article:nth-child(2) {
+    margin-left: 2rem;
+  }
+
+  article:nth-child(4) {
+    margin-right: 0;
+  }
+
+  img {
+    grid-column: 2;
+    grid-row: 1;
+    width: calc(3 * var(--step-0));
+    height: calc(3 * var(--step-0));
+    object-fit: contain;
+    justify-self: end;
+  }
+
+  strong {
+    grid-column: 1;
+    grid-row: 1;
+    font-size: var(--step-5);
+  }
+
+  p {
+    font-size: var(--step-0);
+    grid-column: span 2;
+    align-self: end;
+    margin: 0;
+  }
+</style>
+
+<article style="--bg: var(--{background})">
+  <img src="{energyType}.svg" alt="{energyType} icon" />
+  <strong>{amount}</strong>
+  <p>Average kWp proceeds per {energyType} project</p>
+</article>
---
+++ src/components/molecules/FactList.svelte
@@ -0,0 +1,21 @@
+<script>
+  import FactCard from '../atoms/FactCard.svelte'
+</script>
+
+<style>
+  section {
+    display: flex;
+    align-items: center;
+  }
+
+  h3 {
+    font-size: var(--step-0);
+  }
+</style>
+
+<section>
+  <h3>Amsterdam</h3>
+  <FactCard energyType="solar" amount={200} />
+  <FactCard energyType="wind" amount={130} />
+  <FactCard energyType="biogas" amount={0} />
+</section>

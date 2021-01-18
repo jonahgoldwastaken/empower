95afb18fe372fa228f57682f9cd2b9b28118f570
Jonah Meijers <jonahmeijers97@gmail.com>
2021-01-18 15:12:12 +0000

    feat: make map zoom on compare page

--- src/utils/zoom.js
+++ src/utils/zoom.js
@@ -1,17 +1,5 @@
-export { getCenterOfMultipleBoxes }
+export { getBBoxes }
 
-function getCenterOfMultipleBoxes(els) {
-  return els
-    .map(d => d.getBBox())
-    .reduce(
-      (acc, curr) => {
-        console.log(acc, curr)
-        return [
-          acc[0] + (curr.x + curr.width / 2),
-          acc[1] + (curr.y + curr.height / 2),
-        ]
-      },
-      [0, 0]
-    )
-    .map(n => n / els.length)
+function getBBoxes(els) {
+  return els.map(d => d.getBBox())
 }
